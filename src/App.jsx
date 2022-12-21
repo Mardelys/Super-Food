352abdc538c51e3d108991c7e524b9362c50f318
mardelys <mardelys1996@gmail.com>
2022-12-21 11:18:04 -0500

    Estructura de carpetas base

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.jsx';
+import '../src/styles/App.css'
+import MenuBannerFooter from '../src/components/MenuBannerFooter';
+
+function App() {
+   return (<div
+className="App">
+   <MenuBannerFooter/>
+</div>); 
+}
+export default App;
