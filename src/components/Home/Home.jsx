5a1270d25acc49d4034b404dfcfc89b92e379f79
mardelys <mardelys1996@gmail.com>
2022-12-27 00:23:41 -0500

    diseño

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,20 +14,23 @@ function Home() {
          <div className="about-us">
             <p className="p-home">       Gustosos ovejitos es un negocio de comida rápida con una gran historia detrás, inicialmente llevaba como nombre “Date un gusto” era el sitio preferido de muchas personas para compartir con familia y amigos, pero se decidió hacer una fusión con otro gran exponente de comida rapida en Cabimas, conocido como “Ovejo’s Burguer” de allí nació Gustosos Ovejitos.
             </p>
+
             <img src={Imglogos} className="logo_fusion" alt="Logo Gustosos Ovejitos" />
-            <p className="p-home">Somos más que un puesto de comida rápida, el objetivo es brindar calidad, excelente atención al cliente y sobre todo momentos inolvidables.
-            Prueba nuestros productos y deleitate con el mejor sabor de Cabimas y sobre todo al mejor precio.</p>
+            <div className="p-logo">
+               <p className="p-home">Somos más que un puesto de comida rápida, el objetivo es brindar calidad, excelente atención al cliente y sobre todo momentos inolvidables.
+               Prueba nuestros productos y deleitate con el mejor sabor de Cabimas y sobre todo al mejor precio.</p>
+            
+               <img src={LogoPrincipal} className="lgfusion" alt="Logo Gustosos Ovejitos" />
+            </div>
          </div>
-         <div className="logo-buttons">
-            <img src={LogoPrincipal} className="lgfusion" alt="Logo Gustosos Ovejitos" />
+         
             <h2 className="title-2">¡Bienvenido, Disfruta de nuestros produtos.!</h2>
-            <Link to="/category"><button className='btn-product fixed'>Haz tu Pedido</button></Link>
-         </div>
-         <div>
+         <div className="container-img">
          <img  className="img-food" src={require("./images/home1.png")} alt="" />
          <img  className="img-food" src={require("./images/home2.png")} alt="" />
          <img  className="img-food" src={require("./images/home3.png")} alt="" />
          </div>
+         <Link to="/category"><button className='btn-product fixed'>Haz tu Pedido</button></Link>
       </section>
       </>
    )}
